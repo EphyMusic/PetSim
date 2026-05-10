3e74997d7af1ce312cd0fb316cd86cc002ab29ff
EphyMusic <ephymusicofficial@gmail.com>
2026-05-10 11:32:42 -0700

    -v1

--- main.lua
+++ main.lua
@@ -1,1 +1,10 @@
+---@diagnostic disable: duplicate-set-field
 --Tamagotchi-like
+Pet = require 'modules.pet'
+GS = require 'libs.hump.gamestate'
+GameLoop = require 'states.gameloop'
+
+function love.load()
+    GS.registerEvents()
+    GS.switch(GameLoop)
+end
