724d68f27abbacb7983eaeb758abd29f642929a1
EphyMusic <87033515+EphyMusic@users.noreply.github.com>
2026-05-10 16:06:28 -0700

    Implemented walls.... just visual for now.

--- modules/bounds.lua
+++ modules/bounds.lua
@@ -3,6 +3,7 @@ local Object = require 'libs.classic.classic'
 ---@class Wall
 local Wall = Object:extend()
 
+-- Create a colored rectangular boundary
 function Wall:new(x,y,w,h,r,g,b)
     self.x = x
     self.y = y
@@ -14,7 +15,7 @@ end
 function Wall:draw()
     love.graphics.setColor(unpack(self.color))
     love.graphics.rectangle("fill",self.x,self.y,self.width,self.height)
-    love.graphics.setColor(1,1,1)
+    love.graphics.setColor(1,1,1)  -- Reset to white
 end
 
 return Wall
