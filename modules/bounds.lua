ef65b6e787164a1ac6168bb83464c04b6eb1d284
EphyMusic <87033515+EphyMusic@users.noreply.github.com>
2026-05-10 17:00:19 -0700

    updated wall draw call to push and pop and push and pop and push and...

--- modules/bounds.lua
+++ modules/bounds.lua
@@ -1,3 +1,4 @@
+--v2
 local Object = require 'libs.classic.classic'
 
 ---@class Wall
@@ -13,9 +14,11 @@ function Wall:new(x,y,w,h,r,g,b)
 end
 
 function Wall:draw()
-    love.graphics.setColor(unpack(self.color))
-    love.graphics.rectangle("fill",self.x,self.y,self.width,self.height)
-    love.graphics.setColor(1,1,1)  -- Reset to white
+    love.graphics.push()
+        love.graphics.setColor(unpack(self.color))
+        love.graphics.rectangle("fill",self.x,self.y,self.width,self.height)
+        love.graphics.setColor(1,1,1)  -- Reset to white
+    love.graphics.pop()
 end
 
 return Wall
