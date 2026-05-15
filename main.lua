d092c191bb29b15dc72d346dbd3ddc26df69a53b
EphyMusic <87033515+EphyMusic@users.noreply.github.com>
2026-05-14 21:02:34 -0700

    -forgot to reenable debug.

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@
 -- Pet = require 'modules.pet'
 GS = require 'libs.hump.gamestate'
 GameLoop = require 'states.gameloop'
-bDebug = false  -- Enable debug mode (J/K keys and debug display)
+bDebug = true  -- Enable debug mode (J/K keys and debug display)
 bHandheld = false
 
 function love.load()
